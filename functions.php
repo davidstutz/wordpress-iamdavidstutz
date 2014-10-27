c782fe389ec6ce1c5bfa3082daca0446ff0c8a2a
David Stutz <davidstutz@web.de>
2014-10-27 19:02:30 +0100

    Still working on projects page and getting comments to work again.

--- functions.php
+++ functions.php
@@ -351,25 +351,11 @@ function iamdavidstutz_article_footer() {
     $user = get_user_by('id', $id);
     
     ?>
-    <div class="article-footer">
-        <span class="page-footer-modified">
-            <?php echo __('LASTMODIFIED', 'iamdavidstutz'); ?>
-            <?php $day = get_the_modified_time('d'); ?>
-            <?php if ($day == 1): ?>
-                <?php echo $day; ?><sup>st</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
-            <?php elseif ($day == 2): ?>
-                <?php echo $day; ?><sup>nd</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
-            <?php elseif ($day == 3): ?>
-                <?php echo $day; ?><sup>rd</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
-            <?php else: ?>
-                <?php echo $day; ?><sup>th</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
-            <?php endif; ?>
-        </span>
-    </div>
     <div class="article-author">
-        <?php $query = new WP_query('post_type=ub_part&post_author=' . $user->ID . '&orderby=date&post_limits=1'); ?>
+        <?php $query = new WP_query('post_type=ub_part&post_author=' . $user->ID . '&posts_per_page=1'); ?>
         <?php while ($query->have_posts()): $query->the_post(); ?>
             <blockquote class="author-description">
+                <h3><?php echo __('ABOUTTHE', 'iamdavidstutz'); ?><b><?php echo __('AUTHOR', 'iamdavidstutz'); ?></b></h3>
                 <p><?php the_content(); ?></p>
                 <small>
                     <?php $day = get_the_date('d'); ?>
@@ -381,11 +367,28 @@ function iamdavidstutz_article_footer() {
                         <?php echo $day; ?><sup>rd</sup><?php echo strtoupper(get_the_date('F')); ?><?php echo get_the_date('Y'); ?> 
                     <?php else: ?>
                         <?php echo $day; ?><sup>th</sup><?php echo strtoupper(get_the_date('F')); ?><?php echo get_the_date('Y'); ?>
-                    <?php endif; ?>, <?php echo $user->display_name; ?>
+                    <?php endif; ?>, <a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo $user->display_name; ?></a>
                 </small>
             </blockquote>
         <?php endwhile; ?>
     </div>
+    <!--
+        <div class="article-footer">
+            <small class="text-muted">
+                <?php echo __('LASTMODIFIED', 'iamdavidstutz'); ?>
+                <?php $day = get_the_modified_time('d'); ?>
+                <?php if ($day == 1): ?>
+                    <?php echo $day; ?><sup>st</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
+                <?php elseif ($day == 2): ?>
+                    <?php echo $day; ?><sup>nd</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
+                <?php elseif ($day == 3): ?>
+                    <?php echo $day; ?><sup>rd</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
+                <?php else: ?>
+                    <?php echo $day; ?><sup>th</sup><?php echo strtoupper(get_the_modified_time('F')); ?><?php echo get_the_modified_time('Y'); ?>
+                <?php endif; ?>
+            </small>
+        </div>
+    -->
     <?php
 }
 
