8ac172b40c368ff27d80afccc66b60c3eb1adb4a
David Stutz <davidstutz@web.de>
2017-11-28 22:54:20 +0100

    Sidebar for pages.

--- page.php
+++ page.php
@@ -1,25 +1,31 @@
 <?php get_header(); ?>
 
-    
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()): the_post(); ?>
-            <div class="page">
-                <!--
-                <div class="page-header">
-                    <h2><?php the_title(); ?></h2>
+    <div class="row">
+        <div class="col-md-9">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()): the_post(); ?>
+                    <div class="page">
+                        <!--
+                        <div class="page-header">
+                            <h2><?php the_title(); ?></h2>
+                        </div>
+                        -->
+                        <div class="page-content">
+                            <?php the_content(); ?>
+                        </div>
+                        <?php iamdavidstutz_page_footer(); ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <div class="nothing">
+                    <h1><?php echo __('NOTHING', 'iamdavidstutz'); ?></h1>
+                    <h4><?php echo __('NOTHINGFOUNDHERE', 'iamdavidstutz'); ?></h4>
                 </div>
-                -->
-                <div class="page-content">
-                    <?php the_content(); ?>
-                </div>
-                <?php iamdavidstutz_page_footer(); ?>
-            </div>
-        <?php endwhile; ?>
-    <?php else: ?>
-        <div class="nothing">
-            <h1><?php echo __('NOTHING', 'iamdavidstutz'); ?></h1>
-            <h4><?php echo __('NOTHINGFOUNDHERE', 'iamdavidstutz'); ?></h4>
+            <?php endif; ?>
         </div>
-    <?php endif; ?>
+        <div class="col-md-3">
+            <?php get_sidebar('page'); ?>
+        </div>
+    </div>
     
 <?php get_footer(); ?>
