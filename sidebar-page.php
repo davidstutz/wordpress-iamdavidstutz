00c93b3a66f08fc1e2a5479b47d5535c36c2b8d3
David Stutz <davidstutz@web.de>
2017-12-05 21:03:35 +0100

    Minor fix of update.

--- sidebar-page.php
+++ sidebar-page.php
@@ -1,16 +1,5 @@
 <div class="sidebar">
     <hr class="hidden-lg hidden-md">
-    <div class="sidebar-search">
-        <h5>
-            <span class="sidebar-lead"><?php echo __('SEARCH', 'iamdavidstutz'); ?></span><span class="visible-lg-inline visible-sm-inline visible-xs-inline"><?php echo __('THEBLOG', 'iamdavidstutz'); ?></span>
-        </h5>
-        <?php get_search_form(); ?>
-        <h5 class="pull-right">
-            <?php echo __('FOR', 'iamdavidstutz'); ?><span class="visible-lg-inline visible-sm-inline visible-xs-inline hidden-md-inline"><?php echo __('INTERESTING', 'iamdavidstutz'); ?></span><span class="hidden-xs-inline hidden-sm-inline visible-md-inline hidden-lg-inline"><?php echo __('COOL', 'iamdavidstutz'); ?></span><?php echo __('STUFF', 'iamdavidstutz'); ?>
-        </h5>
-        <div class="clearfix"></div>
-    </div>
-    <hr class="hidden-lg hidden-md">
     <div class="sidebar-donate well well-sm">
         <h5>
         <span class="sidebar-lead"><?php echo __('SUPPORT', 'iamdavidstutz'); ?></span><?php echo __('ME', 'iamdavidstutz'); ?>
@@ -25,6 +14,17 @@
         </div>
         <span class="pull-left small text-muted"><i class="fa fa-paypal"></i>&nbsp;<?php echo __('PayPal', 'iamdavidstutz'); ?></span>
         <a href="http://davidstutz.de/donate/" class="small pull-right"><?php echo __('Why Donate?', 'iamdavidstutz'); ?></a>
+        <div class="clearfix"></div>
+    </div>
+    <hr class="hidden-lg hidden-md">
+    <div class="sidebar-search">
+        <h5>
+            <span class="sidebar-lead"><?php echo __('SEARCH', 'iamdavidstutz'); ?></span><span class="visible-lg-inline visible-sm-inline visible-xs-inline"><?php echo __('THEBLOG', 'iamdavidstutz'); ?></span>
+        </h5>
+        <?php get_search_form(); ?>
+        <h5 class="pull-right">
+            <?php echo __('FOR', 'iamdavidstutz'); ?><span class="visible-lg-inline visible-sm-inline visible-xs-inline hidden-md-inline"><?php echo __('INTERESTING', 'iamdavidstutz'); ?></span><span class="hidden-xs-inline hidden-sm-inline visible-md-inline hidden-lg-inline"><?php echo __('COOL', 'iamdavidstutz'); ?></span><?php echo __('STUFF', 'iamdavidstutz'); ?>
+        </h5>
         <div class="clearfix"></div>
     </div>
 </div>
