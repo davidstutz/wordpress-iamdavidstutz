98a1a2e189501e4b41bf1118d03e0bcd27c289b0
David Stutz <davidstutz@web.de>
2018-04-11 22:42:44 +0200

    Sidebar changes.

--- sidebar-page.php
+++ sidebar-page.php
@@ -1,37 +1,25 @@
-<div style="position:relative">
-    <div class="sidebar" data-spy="affix" data-offset-top="300">
-        <hr class="hidden-lg hidden-md">
-        <div class="sidebar-donate well well-sm">
-            <h5>
-            <span class="sidebar-lead"><?php echo __('SUPPORT', 'iamdavidstutz'); ?></span><?php echo __('ME', 'iamdavidstutz'); ?>
-            </h5>
-            <div class="row">
-                <div class="col-md-offset-1 col-md-5">
-                    <div class="text-center">
-                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <input type="hidden" name="cmd" value="_s-xclick">
-                            <input type="hidden" name="hosted_button_id" value="V95Q7QK6JY32Q">
-                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                            <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-                            <span class="small text-muted"><i class="fa fa-paypal"></i>&nbsp;<?php echo __('PayPal', 'iamdavidstutz'); ?></span>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    <div class="text-center">
-                        <a href="https://flattr.com/@davidstutz" target="_blank"><?php echo __('Flattr', 'iamdavidstutz'); ?></a>
-                    </div>
-                </div>
-            </div>
-            <a href="http://davidstutz.de/donate/" class="small pull-right"><?php echo __('Why Donate?', 'iamdavidstutz'); ?></a>
-            <div class="clearfix"></div>
-        </div>
-        <hr class="hidden-lg hidden-md">
-        <div class="sidebar-search">
-            <h5>
-                <span class="sidebar-lead"><?php echo __('SEARCH', 'iamdavidstutz'); ?></span><span class="visible-lg-inline visible-sm-inline visible-xs-inline"><?php echo __('THEBLOG', 'iamdavidstutz'); ?></span>
-            </h5>
-            <?php get_search_form(); ?>
-        </div>
+<div class="sidebar">
+    <div class="sidebar-search">
+        <h5>
+            <span class="sidebar-lead"><?php echo __('SEARCH', 'iamdavidstutz'); ?></span><span class="visible-lg-inline visible-sm-inline visible-xs-inline"><?php echo __('THEBLOG', 'iamdavidstutz'); ?></span>
+        </h5>
+        <?php get_search_form(); ?>
     </div>
+    <hr class="hidden-lg hidden-md">
+    <div class="sidebar-header">
+        <h4><?php echo __('ARCHIVES', 'iamdavidstutz'); ?></h4>
+        <h4 style="margin-left:30%;"><?php echo __('TAGS', 'TAGS'); ?></h4>
+    </div>
+    <div class="sidebar-archives">
+        <?php echo iamdavidstutz_get_archives(); ?>
+    </div>
+    <div class="sidebar-tags">
+        <ul class="list-unstyled">
+            <?php $tags = get_tags(); ?>
+            <?php foreach ($tags as $tag): ?>
+                <li><a href="<?php echo get_tag_link($tag->term_id); ?>"><span class="label label-primary"><?php echo strtoupper($tag->name); ?></span></a></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <hr class="hidden-lg hidden-md">
 </div>
--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,4 @@
 <div class="sidebar">
-    <hr class="hidden-lg hidden-md">
-    <div class="sidebar-donate well well-sm">
-        <h5>
-        <span class="sidebar-lead"><?php echo __('SUPPORT', 'iamdavidstutz'); ?></span><?php echo __('ME', 'iamdavidstutz'); ?>
-        </h5>
-        <div class="row">
-            <div class="col-md-offset-1 col-md-5">
-                <div class="text-center">
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                        <input type="hidden" name="cmd" value="_s-xclick">
-                        <input type="hidden" name="hosted_button_id" value="V95Q7QK6JY32Q">
-                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                        <img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
-                        <span class="small text-muted"><i class="fa fa-paypal"></i>&nbsp;<?php echo __('PayPal', 'iamdavidstutz'); ?></span>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <div class="text-center">
-                    <a href="https://flattr.com/@davidstutz" target="_blank"><?php echo __('Flattr', 'iamdavidstutz'); ?></a>
-                </div>
-            </div>
-        </div>
-        <a href="http://davidstutz.de/donate/" class="small pull-right"><?php echo __('Why Donate?', 'iamdavidstutz'); ?></a>
-        <div class="clearfix"></div>
-    </div>
-    <hr class="hidden-lg hidden-md">
     <div class="sidebar-search">
         <h5>
             <span class="sidebar-lead"><?php echo __('SEARCH', 'iamdavidstutz'); ?></span><span class="visible-lg-inline visible-sm-inline visible-xs-inline"><?php echo __('THEBLOG', 'iamdavidstutz'); ?></span>
