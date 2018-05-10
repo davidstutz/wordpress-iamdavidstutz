7ddd088b15b1bbf9cee9764e26d37e75a301d9e1
davidstutz <davidstutz@web.de>
2018-05-10 20:40:56 +0200

    GDPR data protection link.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
             <hr>
             <div class="footer text-muted pull-right">
-                <?php echo __('COPYRIGHT', 'iamdavidstutz'); ?> <?php echo __('DAVIDSTUTZ', 'iamdavidstutz'); ?> <a href="http://davidstutz.de/impressum-legal-notice/">IMPRESSUM &mdash; LEGAL NOTICE</a>
+                <?php echo __('COPYRIGHT', 'iamdavidstutz'); ?> <?php echo __('DAVIDSTUTZ', 'iamdavidstutz'); ?> <a href="http://davidstutz.de/impressum-legal-notice/">IMPRESSUM &amp; DATENSCHUTZ</a>
             </div>
         </div>
 		<?php wp_footer(); ?>
