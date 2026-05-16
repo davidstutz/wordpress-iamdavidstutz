4081485b4861c661861d54795df41343e791894c
David Stutz <davidstutz@web.de>
2026-05-16 19:07:04 +0100

    Update footer.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
             <hr>
             <div class="footer text-muted pull-right">
-                <?php echo __('COPYRIGHT', 'iamdavidstutz'); ?> <?php echo __('DAVIDSTUTZ', 'iamdavidstutz'); ?> <a href="https://davidstutz.de/impressum/">IMPRESSUM</a> &amp; <a href="https://davidstutz.de/datenschutz/">DATENSCHUTZ</a>
+                <?php echo __('COPYRIGHT', 'iamdavidstutz'); ?> <?php echo __('DAVIDSTUTZ', 'iamdavidstutz'); ?> <a href="https://davidstutz.de/legal-notice/">LEGAL NOTICE / PRIVACY NOTICE</a>
             </div>
         </div>
 		<?php wp_footer(); ?>
